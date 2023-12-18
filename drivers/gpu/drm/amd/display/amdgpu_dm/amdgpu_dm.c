67476a9852570718c6f4585add774c7aa622cf69
Thomas Crider <gloriouseggroll@gmail.com>
2023-12-18 03:36:09 -0500

    revert 1101185bc50f5e45b8b89300914d9aa35a0c8cbe

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6673,6 +6673,8 @@ create_stream_for_sink(struct drm_connector *connector,
 
 	if (recalculate_timing)
 		drm_mode_set_crtcinfo(&saved_mode, 0);
+	else if (!old_stream)
+		drm_mode_set_crtcinfo(&mode, 0);
 
 	/*
 	 * If scaling is enabled and refresh rate didn't change
@@ -7369,8 +7371,6 @@ enum drm_mode_status amdgpu_dm_connector_mode_valid(struct drm_connector *connec
 		DRM_ERROR("dc_sink is NULL!\n");
 		goto fail;
 	}
-
-	drm_mode_set_crtcinfo(mode, 0);
 
 	stream = create_validate_stream_for_sink(aconnector, mode,
 						 to_dm_connector_state(connector->state),
