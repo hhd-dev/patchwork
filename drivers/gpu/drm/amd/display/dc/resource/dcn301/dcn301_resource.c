0380a8798ede86bec6facdd6dc40ef52b785c337
Joshua Ashton <joshua@froggi.es>
2024-02-13 18:28:41 +0000

    drm/amd/display: Enable 3 overlay planes for Steam Deck
    Edited by Antheas: lets keep this cutie Steam Deck only.

--- drivers/gpu/drm/amd/display/dc/resource/dcn301/dcn301_resource.c
+++ drivers/gpu/drm/amd/display/dc/resource/dcn301/dcn301_resource.c
@@ -23,7 +23,7 @@
  *
  */
 
-
+#include <linux/dmi.h>
 #include "dm_services.h"
 #include "dc.h"
 
@@ -1403,6 +1403,22 @@ static struct resource_funcs dcn301_res_pool_funcs = {
 	.patch_unknown_plane_state = dcn20_patch_unknown_plane_state
 };
 
+static const struct dmi_system_id is_steamdeck[] = {
+	{	/* Valve Steam Deck (Galileo) */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Valve"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "Galileo"),
+		},
+	},
+	{	/* Valve Steam Deck (Jupiter) */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Valve"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "Jupiter"),
+		},
+	},
+	{}
+};
+
 static bool dcn301_resource_construct(
 	uint8_t num_virtual_links,
 	struct dc *dc,
@@ -1436,9 +1452,17 @@ static bool dcn301_resource_construct(
 	dc->caps.max_cursor_size = 256;
 	dc->caps.min_horizontal_blanking_period = 80;
 	dc->caps.dmdata_alloc_size = 2048;
-	dc->caps.max_slave_planes = 2;
-	dc->caps.max_slave_yuv_planes = 2;
-	dc->caps.max_slave_rgb_planes = 2;
+	
+	if (dmi_first_match(is_steamdeck)) {
+		dc->caps.max_slave_planes = 3;
+		dc->caps.max_slave_yuv_planes = 3;
+		dc->caps.max_slave_rgb_planes = 3;
+	} else {
+		dc->caps.max_slave_planes = 2;
+		dc->caps.max_slave_yuv_planes = 2;
+		dc->caps.max_slave_rgb_planes = 2;
+	}
+
 	dc->caps.is_apu = true;
 	dc->caps.post_blend_color_processing = true;
 	dc->caps.force_dp_tps4_for_cp2520 = true;
